3dce186934741d6feea26605e71f44a16d406b06
James Shelander <jeshelander@crimson.ua.edu>
2019-03-15 14:57:00 -0500

    Theme is finally working

--- Bits-and-Bites/App_Start/BundleConfig.cs
+++ Bits-and-Bites/App_Start/BundleConfig.cs
@@ -9,22 +9,22 @@ namespace Bits_and_Bites
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/js/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Content/js/bootstrap/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap/bootstrap.min.css",
+                      "~/Content/style.css"));
         }
     }
 }
