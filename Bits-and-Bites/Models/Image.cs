9483cdbf31a3af77313e533a34a9babb55240766
James Shelander <jeshelander@crimson.ua.edu>
2019-03-31 18:12:31 -0500

    Added models: Image, Ingredients, and Recipie.  Also added a notes page to communicate changes from the original db.

---
+++ Bits-and-Bites/Models/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bits_and_Bites.Models
+{
+    public class Image
+    {
+        int Id { get; set; }
+        string ImageName { get; set; }
+        string ImageAlt { get; set; }
+        double UsageData { get; set; }
+        string ContentType { get; set; }
+    }
+}
